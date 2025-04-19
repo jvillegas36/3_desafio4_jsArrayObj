53516a55f7ff36ea39b717e28b751d107bb3104c
choche36 <jvillegasn@gmail.com>
2025-04-19 17:57:25 -0400

    carga inicial

---
+++ assets/js/script01.js
@@ -0,0 +1,276 @@
+const propiedades_venta = [
+  {
+    nombre: "Apartamento de lujo en zona exclusiva",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    descripcion:
+      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: 4,
+    baño: 4,
+    costo: 5000,
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: "Apartamento acogedor en la montaña",
+    src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+    descripcion:
+      "Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas",
+    ubicacion: "789 Mountain Road, Summit Peaks, CA 23456",
+    habitaciones: 2,
+    baño: 2,
+    costo: 1200,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Penthouse de lujo con terraza panorámica",
+    src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+    descripcion:
+      "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+    ubicacion: "567 Skyline Avenue, Skyview City, CA 56789",
+    habitaciones: 3,
+    baño: 3,
+    costo: 4500,
+    smoke: false,
+    pets: true,
+    },
+  
+  
+    {
+        nombre: "Casa Remodelada Av. San Carlos De Apoquindo",
+        src: "./assets/img/casa1.webp",
+        descripcion:
+          "Casa de 152 metros cudrado, con psicina, ",
+        ubicacion: "San Carlos de Apoquindo, Las Condes, Las Condes. Ubicacion estrategica en pleno Barrio San Carlos de Apoquindo.",
+        habitaciones: 4,
+        baño: 3,
+        costo: 6500,
+        smoke: false,
+        pets: false,
+      },
+      {
+        nombre: "Casa En Venta De 3 Dorm. En Piedra Roja Chicureo Colina",
+        src: "./assets/img/casa2.webp",
+        descripcion:
+          "Linda casa, muy bien mantenida, estilo mediterráneo, ubicada en condominio consolidado, con parque y juegos infantiles, cercana a colegios y comercio en el sector de Piedra Roja de Chicureo.",
+        ubicacion: "Chicureo/piedra Roja, Chicureo, Colina, RM (Metropolitana)",
+        habitaciones: 3,
+        baño: 3,
+        costo: 5790,
+        smoke: true,
+        pets: true,
+      },
+      {
+        nombre: "Casa Ideal Para Remodelar En Sector Nueva Costanera",
+        src: "./assets/img/casa3.webp",
+        descripcion:
+          "Casa para remodelar en pasaje con orientación oriente.",
+        ubicacion: "Gral John O'Brien, Vitacura",
+        habitaciones: 4,
+        baño: 3,
+        costo: 2200,
+        smoke: true,
+        pets: true,
+      },
+];
+
+
+
+const propiedades_alquiler = [
+  {
+    nombre: "Apartamento en el centro de la ciudad",
+    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
+    descripcion:
+      "Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.",
+    ubicacion: "123 Main Street, Anytown, CA 91234",
+    habitaciones: 2,
+    baño: 2,
+    costo: 2000,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Apartamento luminoso con vista al mar",
+    src: "https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    descripcion:
+      "Este hermoso apartamento ofrece una vista impresionante al mar",
+    ubicacion: "456 Ocean Avenue, Seaside Town, CA 56789",
+    habitaciones: 3,
+    baño: 3,
+    costo: 2500,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Condominio moderno en zona residencial",
+    src: "https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    descripcion:
+      "Este elegante condominio moderno está ubicado en una tranquila zona residencial",
+    ubicacion: "123 Main Street, Anytown, CA 91234",
+    habitaciones: 2,
+    baño: 2,
+    costo: 2200,
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: "Apartamento Cabaña BosqueNevado",
+    src: "./assets/img/cab1.jpg",
+    descripcion:
+      "Bosque Nevado, cuenta con 3 cabañas cómodamente implementadas, así garantizar su comodidad, el entorno invita a salir a descubrir paisajes, sonidos, olores y sonidos y lo que implica un contacto intimo con la naturaleza. A pocos minutos del centro de Ski Corralco, centros termales y restaurantes, osea todas las condiciones para que tu estadía sea inolvidable.",
+    ubicacion: "Ruta 181 KM 90.3, 4700000 Malalcahuello, Chile",
+    habitaciones: 2,
+    baño: 2,
+    costo: 3600,
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Refugios Puerto Fuy, Huilo Huilo",
+    src: "./assets/img/ref1.jpg",
+    descripcion:
+      "Espectacular entorno, cabaña equipada, atención de las personas",
+    ubicacion: "Camino Intenacional CH 203, 5210000 Puerto Fuy, Chile",
+    habitaciones: 2,
+    baño: 2,
+    costo: 1820,
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Apart Hotel Bauerle & Apartamentos",
+    src: "./assets/img/apart1.jpg",
+    descripcion:
+      "Apart Hotel Bauerle & Apartamentos está en Temuco y cuenta con vistas a la ciudad y terraza. Se ofrece wifi gratis y parking privado en el propio alojamiento.El alojamiento está equipado con TV de pantalla plana y baño privado con ducha, bañera y artículos de aseo gratuitos. La cocina dispone de nevera, microondas y minibar.",
+    ubicacion: "636 Claro Solar, 4791120 Temuco, Chile",
+    habitaciones: 3,
+    baño: 2,
+    costo: 1800,
+    smoke: true,
+    pets: false,
+  },
+];
+
+const funFumar = (valor) => {
+  let html;
+  if (valor == true) {
+    html =
+      "<p class='text-success'> <i class='fas fa-smoking'></i> Permitido fumar </p>";
+  } else {
+    html =
+      "<p class='text-danger'> <i class='fas fa-smoking-ban'></i> No se permite fumar</p>";
+  }
+  return html;
+};
+
+const funMascota = (valor) => {
+  let html;
+  if (valor == true) {
+    html =
+      "<p class='text-success'> <i class='fas fa-paw'></i> Mascotas permitidas </p>";
+  } else {
+    html =
+      " <p class='text-danger'> <i class='fa-solid fa-ban'></i> No se permiten mascotas</p>";
+  }
+  return html;
+};
+
+
+let html = "";
+
+const htmlVenta = (max = 3) => {
+    html = ""
+  for (let vent of propiedades_venta.slice(0, max)) {
+    html += `
+        <div class='col-md-4 mb-4'>
+            <div class='card'>
+              <img
+                src="${vent.src}"
+                class="card-img-top"
+                alt="Imagen del departamento"
+              />
+              <div class="card-body">
+                <h5 class="card-title">
+                  ${vent.nombre}
+                </h5>
+                <p class="card-text">
+                  ${vent.descripcion}
+                </p>
+                <p>
+                  <i class="fas fa-map-marker-alt"></i> ${vent.ubicacion}
+                </p>
+                <p>
+                  <i class="fas fa-bed"></i> ${vent.habitaciones} Habitaciones |
+                  <i class="fas fa-bath"></i> ${vent.baño} Baños
+                </p>
+                <p><i class="fas fa-dollar-sign"></i> ${Intl.NumberFormat().format(vent.costo)}</p>
+                ${funFumar(vent.smoke)}
+                ${funMascota(vent.pets)}
+              </div>
+            </div>
+          </div>
+    `;
+}
+return html
+};
+
+const elementoVentaRes1 = document.querySelector("#ventaRes1");
+if (elementoVentaRes1) {
+  elementoVentaRes1.innerHTML = htmlVenta(3);
+}
+
+const elementoVentaRes2 = document.querySelector("#ventaRes2");
+if (elementoVentaRes2) {
+  elementoVentaRes2.innerHTML = htmlVenta(6);
+}
+
+
+let html2 = "";
+const htmlAlquiler = (max = 3) => {
+  html = ""
+  for (let vent of propiedades_alquiler.slice(0, max)) {
+    html2 += `
+    <div class='col-md-4 mb-4'>
+            <div class='card'>
+              <img
+                src="${vent.src}"
+                class="card-img-top"
+                alt="Imagen del departamento"
+              />
+              <div class="card-body">
+                <h5 class="card-title">
+                  ${vent.nombre}
+                </h5>
+                <p class="card-text">
+                  ${vent.descripcion}
+                </p>
+                <p>
+                  <i class="fas fa-map-marker-alt"></i> ${vent.ubicacion}
+                </p>
+                <p>
+                  <i class="fas fa-bed"></i> ${vent.habitaciones} Habitaciones |
+                  <i class="fas fa-bath"></i> ${vent.baño} Baños
+                </p>
+                <p><i class="fas fa-dollar-sign"></i> ${Intl.NumberFormat().format(vent.costo)}</p>
+                ${funFumar(vent.smoke)}
+                ${funMascota(vent.pets)}
+              </div>
+            </div>
+          </div>
+    `;
+  }
+  return html2;
+}
+
+const elementoAlqRes1 = document.querySelector("#alqRes1");
+if (elementoAlqRes1) {
+  elementoAlqRes1.innerHTML = htmlAlquiler(3);
+}
+
+const elementoAlqRes2 = document.querySelector("#alqRes2");
+if (elementoAlqRes2) {
+  elementoAlqRes2.innerHTML = htmlAlquiler(6);
+}
+
+
